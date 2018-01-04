a64a62998f6305c3b65e91a08db3e48d8ff60f34
jiguangshen <j.stevenshen@gmail.com>
2018-01-03 22:48:05 -0800

    add pattern class

--- NeuralNetwork/pattern.cpp
+++ NeuralNetwork/pattern.cpp
@@ -2,3 +2,97 @@
 // Created by jiguangshen on 1/3/18.
 //
 
+#include "pattern.h"
+
+// constructor that takes just sizes of input and output patterns
+Pattern::Pattern(int in_size, int out_size) {
+    in_set.resize(static_cast<unsigned int>(in_size));
+    out_set.resize(static_cast<unsigned int>(out_size));
+}
+
+// read data from a file stream
+Pattern::Pattern(int in_size, int out_size, std::ifstream &infile) {
+    in_set.resize(static_cast<unsigned int>(in_size));
+    out_set.resize(static_cast<unsigned int>(out_size));
+
+    load_from_file(infile);
+}
+
+// read data from variable list
+Pattern::Pattern(int in_size, int out_size, int data_id, ...) {
+    in_set.resize(static_cast<unsigned int>(in_size));
+    out_set.resize(static_cast<unsigned int>(out_size));
+
+    id = data_id;
+    va_list vl;
+
+    va_start(vl, data_id);
+    for (auto in : in_set) {
+        in = va_arg(vl, double);
+    }
+
+    for (auto out : out_set) {
+        out = va_arg(vl, double);
+    }
+
+    va_end(vl);
+}
+
+// finally we can load from vector
+Pattern::Pattern(int in_size, int out_size, int data_id, std::vector<double> &input_set,
+                 std::vector<double> &output_set) {
+    in_set.resize(static_cast<unsigned int>(in_size));
+    out_set.resize(static_cast<unsigned int>(out_size));
+
+    id = data_id;
+
+    in_set = input_set;
+    out_set = output_set;
+}
+
+// utility functions to extract pattern values
+double Pattern::in(int id) {
+    return in_set[id];
+}
+
+double Pattern::out(int id) {
+    return out_set[id];
+}
+
+void Pattern::set_in(int id, double value) {
+    in_set[id] = value;
+}
+
+void Pattern::set_out(int id, double value) {
+    out_set[id] = value;
+}
+
+int Pattern::in_size() {
+    return static_cast<int>(in_set.size());
+}
+
+int Pattern::out_size() {
+    return static_cast<int>(out_set.size());
+}
+
+int Pattern::get_id() {
+    return id;
+}
+
+void Pattern::save_to_file(std::ofstream &outfile) {
+    outfile << id << "\t";
+    for (const double &pt : in_set) {
+        outfile << pt << "\t";
+    }
+
+    for (auto i = 0; i < out_set.size(); i++) {
+        outfile << out_set[i];
+        if (i != out_set.size() - 1) outfile << "\t";
+    }
+
+    outfile << std::endl;
+}
+
+void Pattern::load_from_file(std::ifstream &infile) {
+    //TODO
+}
--- NeuralNetwork/pattern.h
+++ NeuralNetwork/pattern.h
@@ -5,9 +5,51 @@
 #ifndef TINY_NN_PATTERN_H
 #define TINY_NN_PATTERN_H
 
+#include <vector>
+#include <stdarg.h>
+#include <fstream>
 
-class pattern {
 
+// Pattern class describes the input data and the output data
+// used for training and testing
+// it offers various way to access the data
+
+class Pattern {
+private:
+    std::vector<double> in_set;
+    std::vector<double> out_set;
+    int id;
+
+public:
+    Pattern(int in_size, int out_size);
+
+    Pattern(int in_size, int out_size, int data_id, ...);
+
+    Pattern(int in_size, int out_size, int data_id, std::vector<double> &in_set, std::vector<double> &out_set);
+
+    Pattern(int in_size, int out_size, std::ifstream &infile);
+
+    ~Pattern() {}
+
+    virtual double in(int id);
+
+    virtual double out(int id);
+
+    virtual void set_in(int id, double value);
+
+    virtual void set_out(int id, double value);
+
+    virtual int in_size();
+
+    virtual int out_size();
+
+    virtual void save_to_file(std::ofstream &outfile);
+
+    virtual void load_from_file(std::ifstream &infile);
+
+    virtual int get_id();
+
+    virtual void copy(Pattern &in);
 };
 
 
